22214c969ed2c959975af486d726e4742a4192c9
javi <javi@MSI-GF63-Thin>
2023-07-08 15:13:24 -0600

    Cambios: Tabla donde salen sus permisos

--- src/java/PermitController.java
+++ src/java/PermitController.java
@@ -3,6 +3,11 @@ import com.mycompany.edu.ulatina.hth_db_connetion.EmployeeTO;
 import com.mycompany.edu.ulatina.hth_db_connetion.PermitService;
 import com.mycompany.edu.ulatina.hth_db_connetion.PermitTO;
 import java.io.Serializable;
+import java.sql.Date;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -24,11 +29,15 @@ import org.primefaces.PrimeFaces;
 @SessionScoped
 public class PermitController implements Serializable{
     
+    private Date date;
     private PermitTO selectedPermit = new PermitTO();
     private final PermitService pService = new PermitService();
     private boolean esNuevo;
-    
-    
+
+    public java.sql.Date convertir(java.util.Date date) {
+        return new java.sql.Date(date.getDate());
+    }
+
     public void openNew() {
         this.esNuevo = true;
         this.selectedPermit = new PermitTO();
@@ -52,6 +61,27 @@ public class PermitController implements Serializable{
     public PermitService getpService() {
         return pService;
     }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    public void showPermit(int id) throws Exception {
+
+            this.pService.searchByEmployee(1);
+
+    }
+    
+    public void onClick(Date date) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(date)));
+        System.out.println(format.format(date));
+    }
     
     public void savePermit() throws Exception {
 
@@ -67,7 +97,8 @@ public class PermitController implements Serializable{
         
         if (flag){
             System.out.println("Saving permit");
-            this.pService.insert(this.selectedPermit);
+            //Date day = (java.sql.Date) (java.sql.Date) selectedPermit.getDate();
+            this.pService.insert(3,Date.valueOf("2023-07-14"));
             //---this.servicioUsuario.listarUsuarios();
             //this.listaUsuarios.add(selectedEmployee);//para simular       
             this.esNuevo = false;
@@ -76,6 +107,18 @@ public class PermitController implements Serializable{
         }
 
     }
+    public List<PermitTO> searchByEmployee(int pk){
+        try{
+            return pService.searchByEmployee(pk);
+        }catch(Exception e){
+            e.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Error in retriving th list of employees"));
+
+        }
+        List<PermitTO> list = new ArrayList<>();
+        return list;
+    }
+    
 
 
     
